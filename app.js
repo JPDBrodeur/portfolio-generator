2f1e06d77ee6e7983b9073fb1a1a262370a578e7
JPDBrodeur <jbrodeur001@gmail.com>
2021-09-14 22:01:36 -0400

    generate the HTML file

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-var profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = profileDataArr => {
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-  };
+const profileDataArgs = process.argv.slice(2);
 
-printProfileData(profileDataArgs);
+const [name, github] = profileDataArgs;
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+})
